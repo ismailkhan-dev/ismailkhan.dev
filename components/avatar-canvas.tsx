1cb924d8feadc193c764c981fa1996a1df63502e
viz1er <viz1er@yopmail.com>
2023-11-01 02:52:11 -0400

    add avatar canvas to hero, update hero layout

---
+++ components/avatar-canvas.tsx
@@ -0,0 +1,66 @@
+import {
+    Html,
+    OrbitControls,
+    Preload,
+    useAnimations,
+    useGLTF,
+} from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import * as THREE from "three";
+
+const Avatar = () => {
+    const [index, setIndex] = useState(0);
+    const avatar = useGLTF("/avatar_animation.glb");
+    const { actions, names } = useAnimations(avatar.animations, avatar.scene);
+
+    // set useState to 1 for 5 seconds, then set it back to 0 for
+    useEffect(() => {
+        const currentAction = actions[names[index]];
+        if (currentAction) {
+            currentAction.clampWhenFinished = true;
+            currentAction.loop = THREE.LoopPingPong;
+            currentAction.reset().fadeIn(0.5).play();
+
+            // Set a timeout to change the loop mode after 5 seconds
+            const timeoutId = setTimeout(() => {
+                if (currentAction.isRunning()) {
+                    currentAction.loop = THREE.LoopOnce;
+                }
+            }, 7000);
+
+            return () => {
+                clearTimeout(timeoutId);
+                currentAction.fadeOut(0.5);
+            };
+        }
+    }, [index, actions, names]);
+
+    return (
+        <group>
+            <primitive
+                object={avatar.scene}
+                scale={1.8}
+                position-y={-2}
+                rotation-y={-0.3}
+                position-x={0.4}
+                position-z={1.7}
+                rotation-x={Math.PI / 7}
+            />
+        </group>
+    );
+};
+
+export const AvatarCanvas = () => {
+    return (
+        <Canvas dpr={[0, 2]} className="h-full">
+            <ambientLight intensity={2.1} />
+            <pointLight position={[1, 1, 1]} />
+            <OrbitControls enabled={false} />
+            <Suspense fallback={null}>
+                <Avatar />
+            </Suspense>
+            <Avatar />
+        </Canvas>
+    );
+};
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import { AvatarCanvas } from "./avatar-canvas";
 
 export default function Intro() {
     const { ref } = useSectionInView("Home", 0.5);
@@ -18,11 +19,76 @@ export default function Intro() {
         <section
             ref={ref}
             id="home"
-            className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+            className="h-screen mb-28 max-w-[100rem] mx-auto text-left sm:mb-0 scroll-mt-[100rem] flex flex-col sm:flex-row sm:items-start sm:justify-center p-4 backdrop-blur bg-purple/50"
         >
-            <div className="flex items-center justify-center">
-                <div className="relative">
-                    <motion.div
+            {/* Text and Buttons */}
+            <div className="xl:pt-56 w-3/4">
+                <motion.h1
+                    className="mb-10 mt-4 pl-24 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                >
+                    <span className="font-bold">Hello, I'm Ismail.</span> I'm a{" "}
+                    <span className="font-bold">full-stack developer</span> with{" "}
+                    <span className="font-bold">6 years</span> of experience. I
+                    enjoy building <span className="italic">sites & apps</span>.
+                    My focus is{" "}
+                    <span className="underline">Angular and Java</span>.
+                </motion.h1>
+
+                <motion.div
+                    className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        delay: 0.1,
+                    }}
+                >
+                    <Link
+                        href="#contact"
+                        className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                        onClick={() => {
+                            setActiveSection("Contact");
+                            setTimeOfLastClick(Date.now());
+                        }}
+                    >
+                        Contact me here{" "}
+                        <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                    </Link>
+
+                    <a
+                        className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                        href="/CV.pdf"
+                        download
+                    >
+                        Download CV{" "}
+                        <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+                    </a>
+
+                    <a
+                        className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                        href="https://linkedin.com"
+                        target="_blank"
+                    >
+                        <BsLinkedin />
+                    </a>
+
+                    <a
+                        className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                        href="https://github.com"
+                        target="_blank"
+                    >
+                        <FaGithubSquare />
+                    </a>
+                </motion.div>
+            </div>
+
+            {/* Avatar */}
+            <div className="h-full w-11/12 overflow-visible">
+                <div className="relative h-full overflow-visible">
+                    {/* <span>testing</span> */}
+                    <AvatarCanvas />
+                    {/* <motion.div
                         initial={{ opacity: 0, scale: 0 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{
@@ -37,7 +103,7 @@ export default function Intro() {
                             height="192"
                             quality="95"
                             priority={true}
-                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                            className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
                         />
                     </motion.div>
 
@@ -53,67 +119,9 @@ export default function Intro() {
                         }}
                     >
                         👋
-                    </motion.span>
+                    </motion.span> */}
                 </div>
             </div>
-
-            <motion.h1
-                className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-            >
-                <span className="font-bold">Hello, I'm Ismail.</span> I'm a{" "}
-                <span className="font-bold">full-stack developer</span> with{" "}
-                <span className="font-bold">6 years</span> of experience. I
-                enjoy building <span className="italic">sites & apps</span>. My
-                focus is <span className="underline">Angular and Java</span>.
-            </motion.h1>
-
-            <motion.div
-                className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                    delay: 0.1,
-                }}
-            >
-                <Link
-                    href="#contact"
-                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-                    onClick={() => {
-                        setActiveSection("Contact");
-                        setTimeOfLastClick(Date.now());
-                    }}
-                >
-                    Contact me here{" "}
-                    <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-                </Link>
-
-                <a
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                    href="/CV.pdf"
-                    download
-                >
-                    Download CV{" "}
-                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-                </a>
-
-                <a
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://linkedin.com"
-                    target="_blank"
-                >
-                    <BsLinkedin />
-                </a>
-
-                <a
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://github.com"
-                    target="_blank"
-                >
-                    <FaGithubSquare />
-                </a>
-            </motion.div>
         </section>
     );
 }
