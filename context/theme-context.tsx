a30ff4e68d37d04b2f68a07ffb37fc26a2c34c3f
viz1er <viz1er@yopmail.com>
2023-11-11 00:54:53 -0500

    add persistent theme cookie

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -5,66 +5,97 @@ import React, { useEffect, useState, createContext, useContext } from "react";
 type Theme = "light" | "dark";
 
 type ThemeContextProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 type ThemeContextType = {
-  theme: Theme;
-  toggleTheme: () => void;
+    theme: Theme;
+    toggleTheme: () => void;
+};
+
+// Utility functions for cookies and persistent themes
+const setCookie = (name: string, value: string, days: number) => {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    let cookieString =
+        name + "=" + (value || "") + expires + "; path=/; samesite=lax; Secure";
+
+    // Add domain only if not localhost
+    if (window.location.hostname !== "localhost") {
+        cookieString += "; domain=.ismailkhan.dev";
+    }
+
+    document.cookie = cookieString;
+};
+
+const getCookie = (name: string) => {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0)
+            return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 };
 
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export default function ThemeContextProvider({
-  children,
+    children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("light");
+    const [theme, setTheme] = useState<Theme>("light");
 
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    } else {
-      setTheme("light");
-      window.localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    }
-  };
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem("theme") as Theme | null;
+        const cookieTheme = getCookie("theme");
 
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+        const effectiveTheme =
+            localTheme ||
+            (cookieTheme === "light" || cookieTheme === "dark"
+                ? cookieTheme
+                : "light");
+        setTheme(effectiveTheme);
+        if (effectiveTheme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+        setCookie("theme", effectiveTheme, 365);
+    }, []);
 
-    if (localTheme) {
-      setTheme(localTheme);
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        window.localStorage.setItem("theme", newTheme);
+        setCookie("theme", newTheme, 365);
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
+    };
 
-      if (localTheme === "dark") {
-        document.documentElement.classList.add("dark");
-      }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-    }
-  }, []);
-
-  return (
-    <ThemeContext.Provider
-      value={{
-        theme,
-        toggleTheme,
-      }}
-    >
-      {children}
-    </ThemeContext.Provider>
-  );
+    return (
+        <ThemeContext.Provider
+            value={{
+                theme,
+                toggleTheme,
+            }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    );
 }
 
 export function useTheme() {
-  const context = useContext(ThemeContext);
+    const context = useContext(ThemeContext);
 
-  if (context === null) {
-    throw new Error("useTheme must be used within a ThemeContextProvider");
-  }
+    if (context === null) {
+        throw new Error("useTheme must be used within a ThemeContextProvider");
+    }
 
-  return context;
+    return context;
 }
